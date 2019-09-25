592a52fa211fc3974b8ae4cb6ffeaf6c2d01f57d
choigone <gowoonvv@gmail.com>
2019-09-25 13:42:42 -0400

    create first project

---
+++ MusicPlayer/MusicPlayer/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  MusicPlayer
+//
+//  Created by Goun Choi on 25/09/2019.
+//  Copyright © 2019 Goun Choi. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
