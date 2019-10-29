3b510c1cc758fd3ad07f9d6d65b9cb1f65ad8611
choigone <gowoonvv@gmail.com>
2019-10-29 19:53:17 -0400

    recreate project

---
+++ MusicPlayer/MusicPlayer/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MusicPlayer
+//
+//  Created by Goun Choi on 2019/10/29.
+//  Copyright © 2019 Goun Choi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
